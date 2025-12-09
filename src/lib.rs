d7f9b1676ba28c5a391b8ec0976b704275550601
awarbler <awoodfor@byui.edu>
2025-12-09 17:22:23 -0600

    took out extra line

--- src/lib.rs
+++ src/lib.rs
@@ -172,8 +172,6 @@ mod display_utils;
 pub mod keywords;
 pub mod parser;
 pub mod tokenizer;
-
-
 #[doc(hidden)]
 // This is required to make utilities accessible by both the crate-internal
 // unit-tests and by the integration tests <https://stackoverflow.com/a/44541071/1026>
